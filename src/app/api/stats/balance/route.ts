b4011edb12c54eba24071dce36ff9ef71e7dd25a
AlexanderC3 <alexanderceule@gmail.com>
2024-11-22 00:57:14 +0100

    yet another fix

--- src/app/api/stats/balance/route.ts
+++ src/app/api/stats/balance/route.ts
@@ -25,7 +25,7 @@ export async function GET(request : Request) {
 }
 
 export type GetBalanceStatsResponseType = Awaited<ReturnType<typeof getBalanceStats>>
-export async function getBalanceStats(userId: string, from: Date, to: Date) {
+async function getBalanceStats(userId: string, from: Date, to: Date) {
     const totals = await prisma.transaction.groupBy({
         by: ["type"],
         where: {
