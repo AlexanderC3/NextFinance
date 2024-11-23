616285b5623177b79f957ebf945e30b4023c5ab4
AlexanderC3 <alexanderceule@gmail.com>
2024-11-23 11:20:39 +0100

    removed initialFocius from calendar

--- src/app/(pages)/(dashboard)/_components/CreateTransactionDialog.tsx
+++ src/app/(pages)/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -137,7 +137,8 @@ function CreateTransactionDialog({trigger, type} : Props) {
                                         </FormControl>
                                     </PopoverTrigger>
                                     <PopoverContent className="w-auto p-0">
-                                        <Calendar mode="single" selected={field.value} onSelect={value => {if(!value)return;field.onChange(value)}} initialFocus/>
+                                        {/* <Calendar mode="single" selected={field.value} onSelect={value => {if(!value)return;field.onChange(value)}} initialFocus/> */}
+                                        <Calendar mode="single" selected={field.value} onSelect={value => {if(!value)return;field.onChange(value)}}/>
                                     </PopoverContent>
                                 </Popover>
                                 <FormDescription>
